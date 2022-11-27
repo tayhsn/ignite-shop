e93c4aa3a3c3180d90069664c58b5661a06a1c78
Tayanne <tayh.snovaes@gmail.com>
2022-11-27 16:22:00 -0300

    refactor: components and files names

---
+++ src/components/HeaderWithCart/styles.ts
@@ -0,0 +1,12 @@
+import { styled } from '../../styles';
+
+export const HeaderContainer = styled('header', {
+   width: '100%',
+   maxWidth: 1180,
+
+   margin: '0 auto',
+   padding: '2rem 0',
+
+   display: 'flex',
+   justifyContent: 'space-between',
+});
