2dcb8ebb2f2fbab8cacedd40c42de8ed286a44ab
Tayanne <tayh.snovaes@gmail.com>
2022-11-27 12:03:16 -0300

    feat: update success page

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,70 +1,80 @@
-import { GetServerSideProps } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import Link from 'next/link'
-import Stripe from 'stripe'
-import { stripe } from '../lib/stripe'
-import { ImageContainer, SuccessContainer } from '../styles/pages/success'
+import { GetServerSideProps } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+import Link from 'next/link';
+import Stripe from 'stripe';
+import { stripe } from '../lib/stripe';
+import { ImageContainer, SuccessContainer } from '../styles/pages/success';
 
 interface SuccessProps {
-  customerName: string
-  product: {
-    name: string
-    imageUrl: string
-  }
+   customerName: string;
+   products: {
+      id: string;
+      image: string;
+   }[];
 }
 
-export default function Success({ customerName, product }: SuccessProps) {
-  return (
-    <>
-      <Head>
-        <title>Compra efetuada | Ignite Shop</title>
-        <meta name="robots" content="noindex" />
-      </Head>
+export default function Success({ customerName, products }: SuccessProps) {
+   console.log({ products });
+   return (
+      <>
+         <Head>
+            <title>Compra efetuada | Ignite Shop</title>
+            <meta name='robots' content='noindex' />
+         </Head>
 
-      <SuccessContainer>
-        <h1>Compra Efetuada!</h1>
+         <SuccessContainer>
+            <div>
+               {products.map((item) => (
+                  <ImageContainer key={item.id}>
+                     <Image src={item.image} width={120} height={110} alt='' />
+                  </ImageContainer>
+               ))}
+            </div>
 
-        <ImageContainer>
-          <Image src={product.imageUrl} width={120} height={110} alt="" />
-        </ImageContainer>
+            <h1>Compra efetuada!</h1>
 
-        <p>
-          Uhuul <strong>{customerName}</strong>, sua
-          <strong>{product.name}</strong> já está a caminho da sua casa.
-        </p>
+            <p>
+               Uhuul <strong>{customerName}</strong>, sua compra de {''}
+               <strong>{products.length} camisetas</strong> já está a caminho da
+               sua casa.
+            </p>
 
-        <Link href="/">Voltar ao catálogo</Link>
-      </SuccessContainer>
-    </>
-  )
+            <Link href='/'>Voltar ao catálogo</Link>
+         </SuccessContainer>
+      </>
+   );
 }
 
 export const getServerSideProps: GetServerSideProps = async ({ query }) => {
-  if (!query.session_id)
-    return {
-      redirect: {
-        destination: '/',
-        permanent: false,
+   if (!query.session_id)
+      return {
+         redirect: {
+            destination: '/',
+            permanent: false,
+         },
+      };
+
+   const sessionId = String(query.session_id);
+
+   const session = await stripe.checkout.sessions.retrieve(sessionId, {
+      expand: ['line_items', 'line_items.data.price.product'],
+   });
+
+   const customerName = session.customer_details.name;
+   const products = session.line_items.data.map((item) => {
+      const product = item.price.product as Stripe.Product;
+
+      return {
+         id: item.id,
+         image: product.images[0],
+      };
+   });
+
+   return {
+      props: {
+         customerName,
+         products,
       },
-    }
-
-  const sessionId = String(query.session_id)
-
-  const session = await stripe.checkout.sessions.retrieve(sessionId, {
-    expand: ['line_items', 'line_items.data.price.product'],
-  })
-
-  const customerName = session.customer_details.name
-  const product = session.line_items.data[0].price.product as Stripe.Product
-
-  return {
-    props: {
-      customerName,
-      product: {
-        name: product.name,
-        imageUrl: product.images[0],
-      },
-    },
-  }
-}
+   };
+};
--- src/styles/pages/success.ts
+++ src/styles/pages/success.ts
@@ -1,55 +1,68 @@
-import { styled } from '..'
+import { styled } from '..';
 
 export const SuccessContainer = styled('main', {
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
-  justifyContent: 'center',
-  margin: '0 auto',
-  height: 656,
+   display: 'flex',
+   flexDirection: 'column',
+   alignItems: 'center',
+   justifyContent: 'center',
+   margin: '0 auto',
+   height: 656,
 
-  h1: {
-    fontSize: '$2xl',
-    color: '$gray100',
-  },
+   h1: {
+      fontSize: '$2xl',
+      color: '$gray100',
+   },
 
-  p: {
-    fontSize: '$xl',
-    color: '$gray300',
-    maxWidth: 560,
-    textAlign: 'center',
-    marginTop: '2rem',
-    lineHeight: 1.4,
-  },
+   div: {
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
 
-  a: {
-    marginTop: '5rem',
-    display: 'block',
-    fontSize: '$lg',
-    color: '$green500',
-    textDecoration: 'none',
-    fontWeight: 'bold',
+      marginBottom: '3rem',
+   },
 
-    '&:hover': {
-      color: '$green300',
-    },
-  },
-})
+   p: {
+      fontSize: '$xl',
+      color: '$gray300',
+      maxWidth: 560,
+      textAlign: 'center',
+      marginTop: '2rem',
+      lineHeight: 1.4,
+   },
+
+   a: {
+      marginTop: '5rem',
+      display: 'block',
+      fontSize: '$lg',
+      color: '$green500',
+      textDecoration: 'none',
+      fontWeight: 'bold',
+
+      '&:hover': {
+         color: '$green300',
+      },
+   },
+});
 
 export const ImageContainer = styled('div', {
-  width: '100%',
-  maxWidth: 130,
-  height: 145,
-  background: 'linear-gradient(180deg, #1ea483 0%, #7465d4 100%)',
-  borderRadius: 8,
-  padding: '0.25rem',
-  marginTop: '4rem',
+   width: 140,
+   height: 140,
 
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
+   position: 'relative',
+   zIndex: 1,
 
-  img: {
-    objectFit: 'cover',
-  },
-})
+   display: 'flex',
+   justifyContent: 'center',
+   alignItems: 'center',
+
+   padding: '0.25rem',
+   margin: '0 -26px',
+
+   background: 'linear-gradient(180deg, #1ea483 0%, #7465e4 100%)',
+   borderRadius: '50%',
+   boxShadow: '0px 0px 60px rgba(0, 0, 0, 0.8)',
+
+   img: {
+      objectFit: 'cover',
+   },
+});
