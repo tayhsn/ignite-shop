f5f304e87137c2fe16f0953884969394bea11a20
Tayanne <tayh.snovaes@gmail.com>
2022-12-19 14:55:30 -0300

    fix: image height and performace

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,163 +1,154 @@
-import 'keen-slider/keen-slider.min.css';
-import { useKeenSlider } from 'keen-slider/react';
+import 'keen-slider/keen-slider.min.css'
+import { useKeenSlider } from 'keen-slider/react'
 
-import Stripe from 'stripe';
+import Stripe from 'stripe'
 
-import { GetStaticProps } from 'next';
-import Head from 'next/head';
-import Image from 'next/image';
-import Link from 'next/link';
+import { GetStaticProps } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import Link from 'next/link'
 
-import { Handbag, Trash } from 'phosphor-react';
-import { MouseEvent, ReactElement } from 'react';
-import { HeaderWithCart } from '../components/HeaderWithCart';
-import { CartItem, useCart } from '../contexts/CartContext';
-import { stripe } from '../lib/stripe';
+import { Handbag, Trash } from 'phosphor-react'
+import { MouseEvent, ReactElement } from 'react'
+import { HeaderWithCart } from '../components/HeaderWithCart'
+import { CartItem, useCart } from '../contexts/CartContext'
+import { stripe } from '../lib/stripe'
 import {
-   HomeContainer,
-   Product,
-   ProductFooterDetails,
-} from '../styles/pages/home';
+  HomeContainer,
+  Product,
+  ProductFooterDetails,
+} from '../styles/pages/home'
 
 interface HomeProps {
-   products: CartItem[];
+  products: CartItem[]
 }
 
 export default function Home({ products }: HomeProps) {
-   const { addToCart, removeFromCart, checkIfAlreadyExists } = useCart();
+  const { addToCart, removeFromCart, checkIfAlreadyExists } = useCart()
 
-   const [sliderRef] = useKeenSlider({
-      slides: {
-         perView: 2.6,
-         spacing: 48,
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 2.6,
+      spacing: 48,
+    },
+    rubberband: false,
+    breakpoints: {
+      '(max-width: 1366px)': {
+        slides: {
+          perView: 2.1,
+          spacing: 24,
+        },
       },
-      rubberband: false,
-      breakpoints: {
-         '(max-width: 1366px)': {
-            slides: {
-               perView: 2.1,
-               spacing: 24,
-            },
-         },
-         '(max-width: 768px)': {
-            slides: {
-               perView: 1.1,
-               spacing: 12,
-            },
-         },
+      '(max-width: 768px)': {
+        slides: {
+          perView: 1.1,
+          spacing: 12,
+        },
       },
-   });
+    },
+  })
 
-   const handleAddToCart = (
-      event: MouseEvent<HTMLButtonElement>,
-      product: CartItem
-   ) => {
-      event.preventDefault();
+  const handleAddToCart = (
+    event: MouseEvent<HTMLButtonElement>,
+    product: CartItem
+  ) => {
+    event.preventDefault()
 
-      addToCart(product);
-   };
+    addToCart(product)
+  }
 
-   const handleRemoveFromCart = (
-      event: MouseEvent<HTMLButtonElement>,
-      product: CartItem
-   ) => {
-      event.preventDefault();
+  const handleRemoveFromCart = (
+    event: MouseEvent<HTMLButtonElement>,
+    product: CartItem
+  ) => {
+    event.preventDefault()
 
-      removeFromCart(product);
-   };
+    removeFromCart(product)
+  }
 
-   return (
-      <>
-         <Head>
-            <title>Home | Ignite Shop</title>
-         </Head>
+  return (
+    <>
+      <Head>
+        <title>Home | Ignite Shop</title>
+      </Head>
 
-         <HomeContainer ref={sliderRef} className='keen-slider'>
-            {products.map((product) => (
-               <Link
-                  key={product.id}
-                  href={`/product/${product.id}`}
-                  prefetch={false}
-               >
-                  <Product className='keen-slider__slide'>
-                     <Image
-                        src={product.imageUrl}
-                        width={520}
-                        height={400}
-                        alt=''
-                     />
+      <HomeContainer ref={sliderRef} className='keen-slider'>
+        {products.map((product) => (
+          <Link
+            key={product.id}
+            href={`/product/${product.id}`}
+            prefetch={false}
+          >
+            <Product className='keen-slider__slide'>
+              <Image src={product.imageUrl} width={520} height={480} alt='' />
 
-                     <ProductFooterDetails>
-                        <span>
-                           <strong>{product.name}</strong>
-                           <strong>{product.price}</strong>
-                        </span>
+              <ProductFooterDetails>
+                <span>
+                  <strong>{product.name}</strong>
+                  <strong>{product.price}</strong>
+                </span>
 
-                        {checkIfAlreadyExists(product.id) ? (
-                           <button
-                              onClick={(event) =>
-                                 handleRemoveFromCart(event, product)
-                              }
-                              aria-label='Remover produto da sacola'
-                           >
-                              <Trash size={24} />
-                           </button>
-                        ) : (
-                           <button
-                              onClick={(event) =>
-                                 handleAddToCart(event, product)
-                              }
-                              aria-label='Adicionar produto na sacola'
-                           >
-                              <Handbag size={24} />
-                           </button>
-                        )}
-                     </ProductFooterDetails>
-                  </Product>
-               </Link>
-            ))}
-         </HomeContainer>
-      </>
-   );
+                {checkIfAlreadyExists(product.id) ? (
+                  <button
+                    onClick={(event) => handleRemoveFromCart(event, product)}
+                    aria-label='Remover produto da sacola'
+                  >
+                    <Trash size={24} />
+                  </button>
+                ) : (
+                  <button
+                    onClick={(event) => handleAddToCart(event, product)}
+                    aria-label='Adicionar produto na sacola'
+                  >
+                    <Handbag size={24} />
+                  </button>
+                )}
+              </ProductFooterDetails>
+            </Product>
+          </Link>
+        ))}
+      </HomeContainer>
+    </>
+  )
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-   const response = await stripe.products.list({
-      expand: ['data.default_price'],
-   });
+  const response = await stripe.products.list({
+    expand: ['data.default_price'],
+  })
 
-   const products = response.data.map((product) => {
-      const price = product.default_price as Stripe.Price;
+  const products = response.data.map((product) => {
+    const price = product.default_price as Stripe.Price
 
-      const formattedPrice = new Intl.NumberFormat('pt-BR', {
-         style: 'currency',
-         currency: 'BRL',
-      }).format(price.unit_amount / 100);
+    const formattedPrice = new Intl.NumberFormat('pt-BR', {
+      style: 'currency',
+      currency: 'BRL',
+    }).format(price.unit_amount / 100)
 
-      return {
-         id: product.id,
-         name: product.name,
-         imageUrl: product.images[0],
-         price: formattedPrice,
-         priceNumber: price.unit_amount,
-         defaultPriceId: price.id,
-      };
-   });
+    return {
+      id: product.id,
+      name: product.name,
+      imageUrl: product.images[0],
+      price: formattedPrice,
+      priceNumber: price.unit_amount,
+      defaultPriceId: price.id,
+    }
+  })
 
-   return {
-      props: {
-         products,
-      },
-      revalidate: 60 * 60 * 2, // 60sec * 60min = 1hour * 2 = 2 hours
-   };
-};
+  return {
+    props: {
+      products,
+    },
+    revalidate: 60 * 60 * 2, // 60sec * 60min = 1hour * 2 = 2 hours
+  }
+}
 
 Home.getLayout = function getLayout(page: ReactElement) {
-   return (
-      <>
-         <HeaderWithCart />
+  return (
+    <>
+      <HeaderWithCart />
 
-         {page}
-      </>
-   );
-};
+      {page}
+    </>
+  )
+}
